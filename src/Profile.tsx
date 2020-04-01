b9ca639d2b700a58909259fc71b14cd64406ebfb
Josh Ponelat <jponelat@gmail.com>
2020-04-01 11:13:54 +0200

    add validation and initial state to profile form

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Form, FormField, Button, Box, Heading } from 'grommet'
 import SelectWithBoxes from './SelectWithBoxes'
 
@@ -12,22 +12,34 @@ interface Props {
 }
 
 export default function Profile(props: Props) {
-  const { onSave } = props
-  // const isEdit = !!props.user?.email
-
-
+  const { onSave, user } = props
+  const [formValue, setForm] = useState(user || {roles: ['PetOwner'], email: 'bob@example.com'})
 
   return (
       <Box background="light-2" pad="medium" >
         <Heading level={3}>Create User</Heading>
         <Form
+          validate="blur"
+          value={formValue}
+          onChange={(form: any) => setForm(form.value)}
           onSubmit={(form: any) => onSave(form)}
-          onChange={(form: any) => console.log(JSON.stringify(form))} >
+           >
 
-          <FormField name="full_name" label="Name" />
-          <FormField name="password" label="Password" type="password" />
-          <FormField name="email" label="Email" type="email"/>
-          <FormField defaultValue={['PetSitter']} name="roles" multiple label="Roles" component={SelectWithBoxes} options={['PetSitter', 'PetOwner']} />
+          <FormField required name="full_name" label="Name" />
+          <FormField required name="password" label="Password" type="password" />
+          <FormField required name="email" label="Email" type="email"/>
+          <FormField
+            required
+            name="roles"
+            multiple
+            label="Roles"
+            component={SelectWithBoxes}
+            options={['PetSitter', 'PetOwner']}
+            validate={(val) => {
+              if(!val?.length || val.length < 1)
+                return 'At least one role is required.'
+            }}
+          />
           <Button fill type="submit" primary label="Create User" />
         </Form>
       </Box>
